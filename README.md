48453cfc40881ee1288ae625759e394431af8a46
bollo-omar <bolloaggrey@gmail.com>
2023-07-17 02:28:43 +0000

    revision

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # My title
-- Hello
-- Bye
+## My title2
+# My title3
+#### My title4
+### My title5
--- markdown2html.py
+++ markdown2html.py
@@ -18,20 +18,47 @@ def convert_markdown_to_html(input_file, output_file):
 
     # Read the Markdown file and convert it to HTML
     with open(input_file, encoding="utf-8") as f:
-        html_lines = []
-        for line in f:
-            # Check for Markdown headings
-            match = re.match(r"^(#+) (.*)$", line)
-            if match:
-                heading_level = len(match.group(1))
-                heading_text = match.group(2)
-                html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
-            else:
-                html_lines.append(line.rstrip())
+        markdown_lines = f.readlines()
+
+    html_lines = convert_lines_to_html(markdown_lines)
 
     # Write the HTML output to a file
     with open(output_file, "w", encoding="utf-8") as f:
         f.write("\n".join(html_lines))
+
+def convert_lines_to_html(markdown_lines):
+    """
+    Converts a list of Markdown lines to HTML lines.
+    """
+    html_lines = []
+    inside_list = False
+
+    for line in markdown_lines:
+        # Check for Markdown headings
+        match = re.match(r"^(#+) (.*)$", line)
+        if match:
+            heading_level = len(match.group(1))
+            heading_text = match.group(2)
+            html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
+        else:
+            # Check for unordered list items
+            match = re.match(r"^- (.*)$", line)
+            if match:
+                list_item_text = match.group(1)
+                if not inside_list:
+                    html_lines.append("<ul>")
+                    inside_list = True
+                html_lines.append(f"<li>{list_item_text}</li>")
+            elif inside_list:
+                html_lines.append("</ul>")
+                inside_list = False
+            else:
+                html_lines.append(line.rstrip())
+
+    if inside_list:
+        html_lines.append("</ul>")
+
+    return html_lines
 
 if __name__ == "__main__":
     # Check that the correct number of arguments were provided
@@ -48,3 +75,4 @@ if __name__ == "__main__":
 
     # Exit with a successful status code
     sys.exit(0)
+
